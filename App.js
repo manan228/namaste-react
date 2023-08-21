d2cb95575aa6538adaca7dafec50dbfd1dd483f4
Manan Shah <manan228@yahoo.in>
2023-08-22 03:24:52 +0530

    episode - 3 Laying the foundation

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+const ele = <span>React Element</span>;
+
+const title = (
+  <h1>
+    {ele}
+    Namaste React using JSX
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I'm an h1 tag"),
+//     React.createElement("h2", {}, "I'm an h2 tag"),
+//   ]),
+// ]);
 
 // const heading = React.createElement(
 //   "h1",
@@ -18,9 +38,9 @@ const parent = React.createElement("div", { id: "parent" }, [
 //   "Hello World from React!"
 // );
 
-console.log(parent);
+// console.log(parent);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(root);
-root.render(parent);
+// console.log(root);
+// root.render(parent);
